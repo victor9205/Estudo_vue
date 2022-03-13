6b6790f5700a7fb8fc6960e64a5d946470061aed
victor9205 <victorrezende9205@gmail.com>
2022-03-13 16:40:54 -0300

    stating project on wsl ubuntu 20.4 docker

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import "./components/styles/global.css";
+
+import Header from "@/components/Header";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+  
+  <nav>
+    <a href="/"><img alt="WDEV" id="logo" src="https://youtu.be/9uCnudWEtgk">
+
+  </nav>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style scoped>
+header {
+  background-color: var(--color-background-nav);
+  width: 100%;
+  height: 60px;
+}
+</style>
