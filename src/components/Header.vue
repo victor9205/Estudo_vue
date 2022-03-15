9f828c97e8492238268672bc099c890ce95a1dfd
victor9205 <victorrezende9205@gmail.com>
2022-03-15 09:34:33 -0300

    Finalização do Menu mobile e desktop

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,19 @@
 <template>
-  <header>
-  
-  <nav>
-   
-    <a href="/"><img alt="WDEV" id="logo" src="https://i.pinimg.com/originals/57/49/22/5749223920cd0c4c7cc9cd98372188c0.jpg"></a>
-  
-    <img alt="Abrir menu" ide="menu-button" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg" img>
-    
+  <header>  
+  <nav class="container">   
+    <a href="/"><img alt="WDEV" id="logo" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg"></a>
+    <img v-on:click="openMenu" alt="Abrir menu" id="menu-button" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg">
+    <div v-on:click="closeMenu" id= "menu-overlay" v-if="menuActive"></div>
+    <div id= "menu-items" :class="{active:menuActive}">
+      <img alt="Abrir menu" id="menu-logo" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/videos">Vídeo</a></li>
+        <li><a href="/sobre">Sobre</a></li>
+        <li><a href="/contato">Contatos</a></li>
+      </ul>
+    </div>
   </nav>
-
   </header>
 </template>
 
@@ -16,17 +21,113 @@
 export default {
   name: "Header",
   components: {},
+  data(){
+    return {
+      menuActive: false
+    }
+  },
+  methods:{
+    openMenu: function(){
+      this.menuActive = true;
+    },
+    closeMenu: function(){
+      this.menuActive = false;
+    }
+  }
 };
 </script>
 
 <style scoped>
-header {
-  background-color: var(--color-background-nav);
-  width: 100%;
-  height: 60px;
-}
+  header {
+    background-color: var(--color-background-nav);
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  nav {
+    display: flex;
+    justify-content: space-between;
+    height:60px;
+    align-items: center;
 
+  }
   #logo{
-   width: 60px;
+   width: 130px;
+  }
+  #menu-button{
+     width: 30px;
+  }
+
+  #menu-overlay{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 40%;
+    height: 100vh;
+    background-color: #000000;
+    opacity: 0.8;
+   
+  }
+  #menu-logo{
+    width: 110px;
+    margin-top: 30px;
+    margin-bottom: 10px;
+  }
+  #menu-items{
+    position: fixed;
+    top: 0;
+    right: 0;
+    background-color: var(--color-background-nav);
+    width: 60%;
+    height: 100vh;
+    display: none;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  #menu-items.active{
+    display:flex;
+
+  }
+
+  ul{
+    list-style: none;
+  }
+
+  ul li{
+    margin:20px 0px;
+  }
+
+  ul li a{
+    color: var(--color-text-light);
+  }
+
+  @media (min-width: 700px){
+    #menu-button,
+    #menu-logo,
+    #menu-overlay{
+      display: none;
+    }
+
+    #menu-items{
+      display:flex;
+      position:static;
+      height: 60px;
+      width: auto;
+    }
+
+    ul{
+      display: flex;
+      height: 60px;
+      align-items: center;
+    }
+
+    ul li{
+      margin:0;
+      margin-left: 20px;
+    }
   }
 </style>
